5123178c83fa9efb17c463129a59294f0aca3931
ArrogantGao <gaoxuanzhao@gmail.com>
2024-08-17 17:14:16 +0800

    generate d3 figs

---
+++ generate_d3.jl
@@ -0,0 +1,11 @@
+using Graphs, TreeWidthSolver
+using Random
+Random.seed!(1234)
+
+for n in 10:2:30
+    for i in 1:10
+        g = random_regular_graph(n, 3)
+        filename = "d3graphs/n$n-$i.gr"
+        save_graph(g, filename)
+    end
+end
